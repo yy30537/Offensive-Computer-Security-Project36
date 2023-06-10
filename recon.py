8890f0136dd15ffab2fe762c4c77994aba3a48fe
Frank Hu <frankhuyy272@gmail.com>
2023-06-10 15:19:31 +0200

    Update recon.py

--- recon.py
+++ recon.py
@@ -1,8 +1,9 @@
 #scan the local network for devices and gateways
 
-from scapy.all import ARP, Ether, srp
+from scapy.all import ARP, Ether, srp, sniff
 import scapy.all as scapy
 import netifaces as ni
+import csv
 
 interface = 'enp0s8'
 def list_interfaces():
@@ -46,12 +47,32 @@ def scan_network(interface, interval=[1,10]):
                 devices_list.append(device)
     return devices_list
 
+def csv_to_dict(file_name):
+    with open(file_name, mode='r') as infile:
+        reader = csv.reader(infile)
+        mydict = {}
+        for rows in reader:
+            mydict[rows[0]] = rows[1]
+    return mydict
+
+def dictionary_to_csv(file_path, dictionary):
+    with open(file_path, 'wb') as csvfile:
+        writer = csv.writer(csvfile)
+        for key, value in dictionary.items():
+            writer.writerow([key, value])
+
+
 def passive_scan_network(interface):
+    file_name = "passive_scan.csv"
+    try:
+        file = open(file_name, "r")
+        file.close()
+    except:
+        print("file not found, creating file")
+        file = open(file_name, "w")
+        file.close()
+    net_dict = csv_to_dict(file_name)
     print("passive scan")
-    print("create csv file")
-    file = open("network_listen.csv", "w")
-    header = "ip,mac\n"
-    file.write(header)
     print("listening")
     pkg_arp = sniff(filter="arp", count=1, iface=interface)
     ipSource = pkg_arp[0][ARP].psrc
@@ -59,11 +80,16 @@ def passive_scan_network(interface):
     if pkg_arp[0][ARP].op == 2:
         print("ARP Reply")
     elif pkg_arp[0][ARP].op == 1:
-        print("ARP Request")    
-    file.write(ipSource + "," + macSource + "\n")
-    file.close()
+        print("ARP Request") 
+    net_dict[ipSource] = macSource 
+    print(net_dict)
+    dictionary_to_csv(file_name, net_dict)
 
-passive_scan_network(interface)
+
+#check if file exists
+
+#while True:
+    #passive_scan_network(interface)
 
     
 
